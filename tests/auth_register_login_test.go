033cd3ce7d09b9e73d389a9ff0469c93eafdf3e7
daniilles <macales15@mail.ru>
2024-06-20 17:48:22 +0300

    init testing

---
+++ tests/auth_register_login_test.go
@@ -0,0 +1,18 @@
+package tests
+
+import (
+	"sso/tests/suite"
+	"testing"
+)
+
+const (
+	emptyAppID = 0
+	appID      = 1
+	appSecret  = "test-secret"
+
+	passDefaultLen = 10
+)
+
+func TestAuthRegisterLogin_Login_HappyPath(t *testing.T) {
+	ctx, st := suite.New(t)
+}
